a1ef204f29c0291aa1600d9cac0208d040ba7fba
shahrier <hossainshahrier.shm@gmail.com>
2023-01-25 23:09:41 +0600

    Final setup

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 require('dotenv').config();
 require('express-async-errors');
+// Extra security packages
+const helmet = require('helmet')
+const cors = require('cors')
+const xss = require('xss-clean')
+const ratelimit = require('express-rate-limit')
+
 const express = require('express');
 const app = express();
 
@@ -14,7 +20,15 @@ const jobsRouter = require('./routes/jobs')
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
+app.set('trust proxy', 1);
+app.use(ratelimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100
+}))
 app.use(express.json());
+app.use(helmet())
+app.use(cors())
+app.use(xss())
 // extra packages
 
 // routes
